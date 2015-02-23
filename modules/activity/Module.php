8c1c4a8b30c1e4282077635757da279c60ae5b4a
Thijs Zumbrink <thijs@targetmedia.nl>
2015-02-23 11:18:10 +0100

    Defer the update of user.last_login, because that query (somehow) takes 50ms.

--- modules/activity/Module.php
+++ modules/activity/Module.php
@@ -1,21 +1,24 @@
 <?php
 
-    namespace app\modules\activity;
+namespace app\modules\activity;
 
-    use Yii;
-    use yii\db\Expression;
+use Yii;
+use yii\db\Expression;
 
-    class Module extends \yii\base\Module {
+class Module extends \yii\base\Module {
 
-        public function init () {
+    public function init () {
 
-            parent::init();
+        parent::init();
 
-            if (!Yii::$app->user->isGuest) {
+        if (!Yii::$app->user->isGuest) {
+            register_shutdown_function(function(){
+                Yii::trace('Shutdown function to update last_login executing.', __METHOD__);
                 Yii::$app->user->identity->last_login = new Expression('NOW()');
                 Yii::$app->user->identity->save();
-            }
-
+            });
         }
 
     }
+
+}
