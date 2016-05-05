c7e920e74ecdc82130f3c3a7703996826394fff0
Sylvannes Alexandres van der Veen <silvan@datavenia.nl>
2016-05-05 15:42:42 +0200

    Replace old rank structure with RBAC roles

--- views/site/_username.php
+++ views/site/_username.php
@@ -1,28 +1,35 @@
 <?php
-	use yii\helpers\Html;
-	/**
-	 * @var $this yii\web\View
-	 * @var $user app\models\User
-	 */
-	
-	$this->registerCss('.username { color: #0005bc;  }');
-	$this->registerCss('.username.member { font-weight: bold; }');
-	$this->registerCss('.username.submod { color: #118ecf; }');
-	$this->registerCss('.username.mod { color: green; }');
-	$this->registerCss('.username.admin { color: orange; }');
-	
-	$options = [];
-	Html::addCssClass($options, 'username');
-	$user->rank & 2 && Html::addCssClass($options, 'member'); // User objects without this flag could theoretically be guests
-	$user->rank & 4 && Html::addCssClass($options, 'submod');
-	$user->rank & 8 && Html::addCssClass($options, 'mod');
-	$user->rank & 16 && Html::addCssClass($options, 'admin');
 
-    if (isset($link) && !$link) {
-        echo Html::tag('span', Html::encode($user->name), $options);
-    }
-    else {
-        echo Html::a(Html::encode($user->name), ['/user', 'id' => $user->id], $options);
-    }
+use app\models\User;
+use yii\helpers\Html;
+
+/**
+ * @var $this yii\web\View
+ * @var $user app\models\User
+ */
+
+$this->registerCss('.username { color: #0005bc;  }');
+$this->registerCss('.username.member { font-weight: bold; }');
+$this->registerCss('.username.mod { color: green; }');
+$this->registerCss('.username.admin { color: orange; }');
+
+$options = [];
+Html::addCssClass($options, 'username');
+if ($user->hasRole(User::ROLE_ADMIN)) {
+    Html::addCssClass($options, 'admin');
+}
+elseif ($user->hasRole(User::ROLE_MOD)) {
+    Html::addCssClass($options, 'mod');
+}
+else {
+    Html::addCssClass($options, 'member');
+}
+
+if (isset($link) && !$link) {
+    echo Html::tag('span', Html::encode($user->name), $options);
+}
+else {
+    echo Html::a(Html::encode($user->name), ['/user', 'id' => $user->id], $options);
+}
 
 ?>
